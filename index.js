22cd28f0b0406301f84dcb5e820ae03e4114ed08
Ismail Vittal <ismailvtl@gmail.com>
2018-11-29 13:35:29 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'));
+const users = require('./data.json');
+
+app.get('/users', (req, res) => {
+  res.json({users:users});
+})
+
+
+app.get('/users/:id', (req, res) => { 
+ var result = users.filter(obj => {
+    return obj.id === Number(req.params.id)
+ })
+ res.json({user:result});
+});
+
+
+app.listen(port, () => console.log(`users app listening on port ${port}!`));
